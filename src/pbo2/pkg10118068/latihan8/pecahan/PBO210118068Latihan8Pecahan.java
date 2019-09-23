198394f52de6f94a94786e8358e7a0d517e9ef94
Satria Aji Putra <satriamaxt@gmail.com>
2019-09-23 19:11:16 +0700

    Initial commit

---
+++ src/pbo2/pkg10118068/latihan8/pecahan/PBO210118068Latihan8Pecahan.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118068.latihan8.pecahan;
+
+/**
+ *
+ * @author
+ * NAMA     : Satria Aji Putra
+ * KELAS    : IF-2
+ * NIM      : 10118068
+ * Deskripsi Program : Latihan mendeklarasikan, mengisi, dan menampilkan
+ * variable pecahan
+ */
+public class PBO210118068Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float: " + f);
+        System.out.println("Double: " + d);
+    }
+    
+}
